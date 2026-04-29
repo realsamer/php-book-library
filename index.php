245b1e40022baf723c7a08c8f747e918efb37427
realsamer <cleversamer@gmail.com>
2026-04-29 13:24:47 +0300

    feat: add initial books and genres arrays

--- index.php
+++ index.php
@@ -2,7 +2,36 @@
 session_start();
 
 // Project: PHP Book Library
-// Phase 0: Basic project setup
+// Phase 1: Add initial books and genres arrays
+
+$genres = ["Fiction", "Non-Fiction", "Science", "History", "Biography", "Technology"];
+
+$books = [
+    [
+        "id" => 1,
+        "title" => "The Silent Patient",
+        "author" => "Alex Michaelides",
+        "genre" => "Fiction",
+        "year" => 2019,
+        "pages" => 336
+    ],
+    [
+        "id" => 2,
+        "title" => "A Brief History of Time",
+        "author" => "Stephen Hawking",
+        "genre" => "Science",
+        "year" => 1988,
+        "pages" => 256
+    ],
+    [
+        "id" => 3,
+        "title" => "Clean Code",
+        "author" => "Robert Martin",
+        "genre" => "Technology",
+        "year" => 2008,
+        "pages" => 464
+    ]
+];
 
 ?>
 <!DOCTYPE html>
@@ -16,6 +45,8 @@ session_start();
 <body>
 
     <h1>PHP Book Library</h1>
+
+    <p>Initial books and genres arrays have been created.</p>
 
 </body>
 
